f7ee8f4f3c1cdb6d79373f6bc7022f9b2f46965b
Zac Crawford <zwcrawford@gmail.com>
2019-03-03 14:19:40 -0600

    added icons

--- fullpage.js
+++ fullpage.js
@@ -1,5 +1,5 @@
 new fullpage('#fullpage', {
-  anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+  anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
   sectionsColor: ['#C0C0C0', '#C0C0C0', '#C0C0C0', '#C0C0C0'], verticalCentered: true,
   fadingEffect: true,
   fadingEffectKey: 'YWx2YXJvdHJpZ28uY29tXzAzN1ptRmthVzVuUldabVpXTjBiNXo='
