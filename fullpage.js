2c1233d8f46e1e9a3f89a924e63fe8326dcf787c
Zac Crawford <zwcrawford@gmail.com>
2019-03-03 07:53:27 -0600

    went a different direction

---
+++ fullpage.js
@@ -0,0 +1,6 @@
+new fullpage('#fullpage', {
+  anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+  sectionsColor: ['#C0C0C0', '#C0C0C0', '#C0C0C0', '#C0C0C0'], verticalCentered: true,
+  fadingEffect: true,
+  fadingEffectKey: 'YWx2YXJvdHJpZ28uY29tXzAzN1ptRmthVzVuUldabVpXTjBiNXo='
+});
